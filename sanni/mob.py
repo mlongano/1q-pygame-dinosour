9c031bd0f465270d660a0a0f746265c6fcc55ee6
mlongano <mauro.longano@marconirovereto.it>
2024-02-29 10:48:57 +0100

    Aggiungi animazione della lumaca

--- sanni/mob.py
+++ sanni/mob.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 from random import randint
 
+
 class Mob(Sprite):
 
     def __init__(self):
@@ -10,6 +11,18 @@ class Mob(Sprite):
         snail_1 = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
         snail_2 = pygame.image.load("graphics/snail/snail2.png").convert_alpha()
 
+        self.snail_frames = [snail_1, snail_2]
+
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 100
+        self.current_frame = 0
+
+        # Modo piu' sintetico di creare i frame:
+        # self.snail_frames = [
+        #    pygame.image.load("graphics/snail/snail1.png").convert_alpha(),
+        #    pygame.image.load("graphics/snail/snail2.png").convert_alpha(),
+        # ]
+
         y_pos = 300
 
         self.image = snail_1
@@ -17,7 +30,15 @@ class Mob(Sprite):
 
     def update(self):
         self.rect.x -= 5
+        self.animate()
         self.destroy()
+
+    def animate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.snail_frames)
+            self.image = self.snail_frames[self.current_frame]
 
     # rimuovere lo sprite quando esce dallo schermo
     def destroy(self):
